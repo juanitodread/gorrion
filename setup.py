f1d63b11ca1c28cbcb6ec13c661d6d8c70027d7e
juanitodread <juanitodread@gmail.com>
2020-09-17 20:46:22 -0700

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+   name='gorrion',
+   version='0.1.0',
+   description='Utility to twit lyrics just for fun',
+   author='Juan Sandoval',
+   author_email='',
+   packages=['gorrion'],
+   install_requires=[], #external packages as dependencies
+   scripts=[]
+)
