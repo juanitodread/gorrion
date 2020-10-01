decfd19b854fe133e712514828533e8186c49d13
Juan Sandoval <juanitodread@gmail.com>
2020-09-30 20:34:32 -0700

    Add Twitter support to tweet the status of the song being played (#3)

--- src/gorrion.py
+++ src/gorrion.py
@@ -1,19 +1,74 @@
+import sys
+import argparse
+
 from src.config import Config
 from src.clients.spotify.client import Spotify
+from src.clients.twitter.client import Twitter
 
 
 class Gorrion:
-    def run(self):
-        spotify = Spotify(
+    def __init__(self) -> None:
+        self._spotify = Spotify(
             Config.SPOTIFY_CLIENT_ID,
             Config.SPOTIFY_CLIENT_SECRET,
             Config.SPOTIFY_REFRESH_TOKEN,
         )
+        self._twitter = Twitter(
+            Config.TWITTER_API_CONSUMER_KEY,
+            Config.TWITTER_API_CONSUMER_SECRET,
+            Config.TWITTER_API_ACCESS_TOKEN,
+            Config.TWITTER_API_ACCESS_TOKEN_SECRET,
+        )
 
-        current_track = spotify.get_current_track()
-        print('CURRENT_TRACK: ', current_track)
+    def playing(self, disable_twitter: bool=False) -> None:
+        current_track = self._spotify.get_current_track()
 
+        current_track_text = ('Now listening 🎶🔊: \n'
+                             f'\nTrack: {current_track.track_number}. {current_track.name}'
+                             f'\nAlbum: {current_track.album.name}'
+                             f'\nArtist: {", ".join([artist.name for artist in current_track.artists])}'
+                             '\n\n#gorrion #NowPlaying'
+                             f'\n\n{current_track.href.replace("api.spotify.com/v1/tracks", "open.spotify.com/track")}')
+
+        print(current_track_text)
+
+        if not disable_twitter:
+            post = self._twitter.post(current_track_text)
+            if post:
+                print('Status synced on Twitter')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Gorrion app')
+    
+    parser.add_argument(
+        'command',
+        type=str,
+        help='The available commands to execute: [playing, lyric].'
+    )
+    parser.add_argument(
+        '-l',
+        '--local',
+        action='store_true',
+        default=False,
+        help='Enables local mode. No Twitter sync.'
+    )
+
+    return parser.parse_args()
 
 if __name__ == "__main__":
+    args = parse_args()
+
+    command = args.command
+    local_mode = args.local
+
+    valid_commands = ('playing', 'lyric')
+    if command not in valid_commands:
+        print(f'Invalid command. Use: {valid_commands}')
+        quit()
+
     gorrion = Gorrion()
-    gorrion.run()
+    if command == 'playing':
+        gorrion.playing(local_mode)
+    elif command == 'lyric':
+        pass
---
+++ tests/clients/twitter/test_client.py
@@ -0,0 +1,35 @@
+from unittest.mock import patch, MagicMock
+
+from src.clients.twitter.client import Twitter
+
+
+class TestTwitter:
+    @patch('src.clients.twitter.client.API')
+    def test_constructor(self, API_mock):
+        twitter = Twitter(
+            'consumer-key',
+            'consumer-secret',
+            'access-token',
+            'access-token-secret',
+        )
+
+        assert twitter._consumer_key == 'consumer-key'
+        assert twitter._consumer_secret == 'consumer-secret'
+        assert twitter._access_token == 'access-token'
+        assert twitter._access_token_secret == 'access-token-secret'
+        assert twitter._client != None
+
+    @patch('src.clients.twitter.client.API')
+    def test_post(self, API_mock):
+        API_mock.return_value.update_status = MagicMock(return_value='Tweet ha sido enviado')
+
+        twitter = Twitter(
+            'consumer-key',
+            'consumer-secret',
+            'access-token',
+            'access-token-secret',
+        )
+
+        status = twitter.post('tweet status')
+
+        assert status == 'Tweet ha sido enviado'
