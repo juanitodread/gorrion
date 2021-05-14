1883426ab8ccf5a50d1bad11f85c03bbe1cd76b6
juanitodread <juanitodread@gmail.com>
2021-05-13 22:18:16 -0700

    Setup flake8

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 
 setup(
-   name='gorrion',
-   version='0.1.0',
-   description='Utility to twit lyrics just for fun',
-   author='Juan Sandoval',
-   author_email='',
-   packages=['gorrion'],
-   install_requires=[],
-   scripts=[],
+    name='gorrion',
+    version='0.1.0',
+    description='Utility to twit lyrics just for fun',
+    author='Juan Sandoval',
+    author_email='',
+    packages=['gorrion'],
+    install_requires=[],
+    scripts=[],
 )
