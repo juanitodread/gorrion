17f490c115f330deccde858a013dbaadd24ba5c4
juanitodread <juanitodread@gmail.com>
2023-08-13 14:08:42 -0700

    Upgrade to Python 3.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='gorrion',
-    version='0.1.0',
+    version='1.3.0',
     description='Utility to twit lyrics just for fun',
     author='Juan Sandoval',
     author_email='',
